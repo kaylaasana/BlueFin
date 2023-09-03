e90ba780c82b0e6a7fa49596a254a380dee52fc4
kayla <kayladatte@gmail.com>
2023-09-03 11:57:14 -0700

    created User model

---
+++ server/models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+const levelSchema = require('./Level');
+const bcrypt = require('bcrypt');
+
+// establish user schema blueprint
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    // set min username length to 4 characters
+    minlength: 4,
+    // set max username length to 10 characters
+    maxlength: 10,
+    // trim white space from front and end of string
+    trim: true,
+  },
+  email: {
+    type: String,
+    // checks is email is unique
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  // reference Levels schema
+  level: [levelSchema]
+});
+
+// set up pre-save middleware to create password
+userSchema.pre("save", async function (next) {
+    // if password is new or modified, salt and hash password
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model("user", userSchema);
+
+module.exports = User;
