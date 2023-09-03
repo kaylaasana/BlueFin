d3ec9a1e0f66bca2c9b50f2e626258a8fc524bd8
moonjunsain <moonjunsain@gmail.com>
2023-09-03 14:30:45 -0700

    added performance window

--- client/src/3D/Experience.jsx
+++ client/src/3D/Experience.jsx
@@ -1,6 +1,7 @@
 import { useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { useRef } from 'react'
+import { Perf } from 'r3f-perf'
 
 export default function Experience()
 {
@@ -14,6 +15,8 @@ export default function Experience()
     return <>
 
         <OrbitControls makeDefault />
+
+        <Perf position={'top-left'}/>
 
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
