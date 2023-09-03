e30aa6a9e6899ccf0e2f188ada21a5f1ec435b89
moonjunsain <moonjunsain@gmail.com>
2023-09-03 11:39:40 -0700

    added router for switching pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.jsx'
+import Homepage from './pages/Homepage.jsx'
 import './index.css'
 
+/**
+ * Router setup
+ */
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <Error/>,
+    children: [
+      {
+        index: true,
+        element: <Homepage/>
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
