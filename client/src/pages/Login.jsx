b4f5c59ca015bfa292fe512b2e989d5b5733a715
kayla <kayladatte@gmail.com>
2023-09-04 13:45:05 -0700

    created login page functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,30 +1,83 @@
 import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutation";
+import { Link } from "react-router-dom";
+import Auth from "../utils/auth";
 
 // create login page component
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [formState, setFormState] = useState({ username: "", password: "" });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  // update the state based on the form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    //set form state based on the value
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  //form submit
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // assign formState to data
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      // assign token to user data at login
+      Auth.login(data.login.token);
+    } catch (error) {
+      console.error(error);
+      throw new Error("Login failed");
+    }
+
+    // reset input fields on submission
+    setFormState({
+      username: "",
+      password: "",
+    });
+  };
 
   // build page layout
   return (
     <div>
       <div className="d-flex justify-content-start">
-        <button>Homepage</button>
+        <Link to='/'><button>Homepage</button></Link>
       </div>
       <div className="col d-flex justify-content-center">
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <label className="row text">
             Username
             <br></br>
-            <input className="row"></input>
+            <input
+              className="row"
+              placeholder="your username"
+              name="username"
+              type="username"
+              value={formState.username}
+              onChange={handleChange}
+            />
           </label>
           <label className="row">
             Password
             <br></br>
-            <input className="row"></input>
+            <input
+              className="row"
+              placeholder="********"
+              name="password"
+              type="password"
+              value={formState.password}
+              onChange={handleChange}
+            />
           </label>
-            <button>Submit</button>
-            <button>Sign Up</button>
+          <button type="submit">Submit</button>
+          <button>Sign Up</button>
         </form>
       </div>
     </div>
--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-// 
+// login mutation
 export const LOGIN_USER = gql`
   mutation login($username: String!, $password: String!) {
     login(username: $username, password: $password) {
