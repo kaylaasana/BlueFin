e82e1d7aaac590728d08695ff2b670e02d51a180
moonjunsain <moonjunsain@gmail.com>
2023-09-03 21:40:01 -0700

    added links to each mesh so that it directs the user to different page on portal

--- client/src/3D/Portal.jsx
+++ client/src/3D/Portal.jsx
@@ -3,14 +3,31 @@ import { extend } from '@react-three/fiber'
 import { geometry } from 'maath'
 import * as THREE from 'three'
 extend(geometry)
-function Frame({ position, children, bg, text, textPosition=[-0.375, 1, 0.01] }){
+
+function Frame({ position, children, bg, text, textPosition=[-0.375, 1, 0.01], rotation=[0, 0, 0], link='/'}){
+
+    const handleClick = (e)=>{
+        window.location.href = link
+    }
+
     return (
-        <group position={position}>
-            <Text fontSize={0.3} anchorY="top" anchorX="left" lineHeight={0.8} position={textPosition} material-toneMapped={false}>
+        <group position={position} rotation={rotation}>
+            <Text 
+                fontSize={0.3} 
+                anchorY="top" 
+                anchorX="left" 
+                lineHeight={0.8} 
+                position={textPosition} 
+                material-toneMapped={false}
+            >
                 { text }
             </Text>
-            <mesh>
-                <roundedPlaneGeometry args={[1, 1.61803398875, 0.1]} />
+            <mesh 
+                onPointerEnter={()=> {document.body.style.cursor = 'pointer'}} 
+                onPointerLeave={()=>{document.body.style.cursor='default'}} 
+                onClick={handleClick}
+            >
+                <roundedPlaneGeometry args={[1.2, 1.61803398875, 0.1]} />
                 <MeshPortalMaterial side={THREE.DoubleSide}>
                     <color attach={'background'} args={ [bg] }/>
                     {children}
@@ -25,7 +42,7 @@ export default function Portal(){
         <OrbitControls/>
 
         {/* Left */}
-        <Frame position={[-2, 0, 0]} bg={'red'} text={'Login'}>
+        <Frame position={[-2, 0, 0]} bg={'red'} text={'Login'} rotation={[0, Math.PI * 0.2, 0]}>
             <mesh scale={0.5}>
                 <boxGeometry/>
                 <meshNormalMaterial/>
@@ -33,7 +50,7 @@ export default function Portal(){
         </Frame>
 
         {/* Middle */}
-        <Frame position={[0, 0, 0]} bg={'red'} text={'Train'}>
+        <Frame position={[0, 0, 0]} bg={'blue'} text={'Train'}>
             <mesh scale={0.5}>
                 <boxGeometry/>
                 <meshNormalMaterial/>
@@ -41,7 +58,7 @@ export default function Portal(){
         </Frame>
 
         {/* Right */}
-        <Frame position={[2, 0, 0]} bg={'red'} text={'Profile'} textPosition={[-0.425, 1, 0.01]}>
+        <Frame position={[2, 0, 0]} bg={'yellow'} text={'Profile'} textPosition={[-0.425, 1, 0.01]} rotation={[0, -Math.PI * 0.2, 0]}>
             <mesh scale={0.5}>
                 <boxGeometry/>
                 <meshNormalMaterial/>
