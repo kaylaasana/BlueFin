95c39052e44cffdf45569b731fe381670cc34c42
Andy Godoy <Andygodoy@ymail.com>
2023-09-04 16:32:13 -0700

    added comments to typedefs, made minor changes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,16 +13,21 @@ type levelSchema {
   levelNumber: Int!
 }
 
+// Query type is defining a single query getUser, allows us to retrieve a user by their userId.
 type Query {
   getUser(userId: ID!): User
 }
 
+// Define mutation type for creating a new user
+// Define delete progress mutation
+// The mutation type defines 'updateUserProgress' mutation to update a user's progress.
 type Mutation {
   createUser(username: String!, email: String!, password: String!): User
   updateUserProgress(userId: ID!, levelName: String!, levelNumber: Int!): User
   deleteUserProgress(userId: ID!): User
 }
 
+// The schema definition specifies the entry points for queries and mutations.
 schema {
   query: Query
   mutation: Mutation
