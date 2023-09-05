acd66198f73d5504f741eac291cb37f88c120747
Jay Liu <jayliu89@gmail.com>
2023-09-04 17:17:17 -0700

    playNote general functionality working

--- client/src/utils/playNote.js
+++ client/src/utils/playNote.js
@@ -1,31 +1,14 @@
 import * as Tone from "tone"
-// import { Piano } from "@tonejs/piano";
 
 const playNote = () => {
   const piano = new Tone.Sampler({
   urls: {
-      //  'A0': 'A0.[mp3|ogg]',
        'A1': 'A1.[mp3|ogg]',
        'A2': 'A2.[mp3|ogg]',
        'A3': 'A3.[mp3|ogg]',
        'A4': 'A4.[mp3|ogg]',
        'A5': 'A5.[mp3|ogg]',
        'A6': 'A6.[mp3|ogg]',
-      //  'A#0': 'As0.[mp3|ogg]',
-      //  'A#1': 'As1.[mp3|ogg]',
-      //  'A#2': 'As2.[mp3|ogg]',
-       'A#3': 'As3.[mp3|ogg]',
-       'A#4': 'As4.[mp3|ogg]',
-       'A#5': 'As5.[mp3|ogg]',
-       'A#6': 'As6.[mp3|ogg]',
-      //  'B0': 'B0.[mp3|ogg]',
-      //  'B1': 'B1.[mp3|ogg]',
-      //  'B2': 'B2.[mp3|ogg]',
-       'B3': 'B3.[mp3|ogg]',
-       'B4': 'B4.[mp3|ogg]',
-       'B5': 'B5.[mp3|ogg]',
-       'B6': 'B6.[mp3|ogg]',
-      //  'C0': 'C0.[mp3|ogg]',
        'C1': 'C1.[mp3|ogg]',
        'C2': 'C2.[mp3|ogg]',
        'C3': 'C3.[mp3|ogg]',
@@ -33,91 +16,39 @@ const playNote = () => {
        'C5': 'C5.[mp3|ogg]',
        'C6': 'C6.[mp3|ogg]',
        'C7': 'C7.[mp3|ogg]',
-      //  'C#0': 'Cs0.[mp3|ogg]',
-      //  'C#1': 'Cs1.[mp3|ogg]',
-      //  'C#2': 'Cs2.[mp3|ogg]',
-      //  'C#3': 'Cs3.[mp3|ogg]',
-      //  'C#4': 'Cs4.[mp3|ogg]',
-       'C#5': 'Cs5.[mp3|ogg]',
-       'C#6': 'Cs6.[mp3|ogg]',
-      //  'D0': 'D0.[mp3|ogg]',
-      //  'D1': 'D1.[mp3|ogg]',
-      //  'D2': 'D2.[mp3|ogg]',
-      //  'D3': 'D3.[mp3|ogg]',
-       'D4': 'D4.[mp3|ogg]',
-       'D5': 'D5.[mp3|ogg]',
-       'D6': 'D6.[mp3|ogg]',
-      //  'D#0': 'Ds0.[mp3|ogg]',
        'D#1': 'Ds1.[mp3|ogg]',
        'D#2': 'Ds2.[mp3|ogg]',
        'D#3': 'Ds3.[mp3|ogg]',
        'D#4': 'Ds4.[mp3|ogg]',
        'D#5': 'Ds5.[mp3|ogg]',
        'D#6': 'Ds6.[mp3|ogg]',
-      //  'E0': 'E0.[mp3|ogg]',
-      //  'E1': 'E1.[mp3|ogg]',
-      //  'E2': 'E2.[mp3|ogg]',
-      //  'E3': 'E3.[mp3|ogg]',
-      //  'E4': 'E4.[mp3|ogg]',
-       'E5': 'E5.[mp3|ogg]',
-       'E6': 'E6.[mp3|ogg]',
-      //  'F0': 'F0.[mp3|ogg]',
-      //  'F1': 'F1.[mp3|ogg]',
-      //  'F2': 'F2.[mp3|ogg]',
-      //  'F3': 'F3.[mp3|ogg]',
-       'F4': 'F4.[mp3|ogg]',
-       'F5': 'F5.[mp3|ogg]',
-       'F6': 'F6.[mp3|ogg]',
-      //  'F#0': 'Fs0.[mp3|ogg]',
        'F#1': 'Fs1.[mp3|ogg]',
        'F#2': 'Fs2.[mp3|ogg]',
        'F#3': 'Fs3.[mp3|ogg]',
        'F#4': 'Fs4.[mp3|ogg]',
        'F#5': 'Fs5.[mp3|ogg]',
        'F#6': 'Fs6.[mp3|ogg]',
-      //  'G0': 'G0.[mp3|ogg]',
-      //  'G1': 'G1.[mp3|ogg]',
-      //  'G2': 'G2.[mp3|ogg]',
-      //  'G3': 'G3.[mp3|ogg]',
-       'G4': 'G4.[mp3|ogg]',
-       'G5': 'G5.[mp3|ogg]',
-       'G6': 'G6.[mp3|ogg]',
-      //  'G#0': 'Gs0.[mp3|ogg]',
-      //  'G#1': 'Gs1.[mp3|ogg]',
-      //  'G#2': 'Gs2.[mp3|ogg]',
-      //  'G#3': 'Gs3.[mp3|ogg]',
-       'G#4': 'Gs4.[mp3|ogg]',
-       'G#5': 'Gs5.[mp3|ogg]',
-       'G#6': 'Gs6.[mp3|ogg]'
     },
-    
-//   baseUrl: "https://tonejs.github.io/audio/salamander/",
-//   onload: () => {
-//     sampler.triggerAttackRelease(["G6"], 5)  
+
+    baseUrl: "https://tonejs.github.io/audio/salamander/",
   }).toDestination();
+
+  Tone.loaded().then(() => {
+    let notes = [
+    'A1','A2','A3','A4','A5','A6','A#1','A#2','A#3','A#4','A#5','A#6',
+    'B1','B2','B3','B4','B5','B6',
+    'C1','C2','C3','C4','C5','C6','C#1','C#2','C#3','C#4','C#5','C#6',
+    'D1','D2','D3','D4','D5','D6','D#1','D#2','D#3','D#4','D#5','D#6',
+    'E1','E2','E3','E4','E5','E6',
+    'F1','F2','F3','F4','F5','F6','F#1','F#2','F#3','F#4','F#5','F#6',
+    'G1','G2','G3','G4','G5','G6','G#1','G#2','G#3','G#4','G#5','G#6'
+  ]
+    
+    let idx = notes[Math.floor(Math.random() * notes.length)];   
+    console.log(idx)
+
+    piano.triggerAttackRelease(idx, 3);
+  })
 }
-
-
-//   console.log(sampler)
-
-// piano({
-//   parent: document.querySelector("#content"),
-//   noteon: note => sampler.triggerAttack(note.name),
-//   noteoff: note => sampler.triggerRelease(note.name),
-
-// });
-
-// Pour ajouter des effets...
-// Exemples..
-// const filter = new Tone.AutoFilter(4).start();
-// const distortion = new Tone.Distortion(0.5);
-
-// const reverb = new Tone.Reverb(10);
-
-// connect the player to the filter, distortion and then to the master output
-// sampler.chain(filter, distortion, reverb, Tone.Destination);
-
-// sampler.chain(reverb, Tone.Destination);
-
 
 export default playNote
