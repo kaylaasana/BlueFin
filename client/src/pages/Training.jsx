8ffc6bf3443bbda718a25416e8e6767084ccb9ba
Jay Liu <jayliu89@gmail.com>
2023-09-05 21:13:14 -0700

    minor cleanup of training component

--- client/src/pages/Training.jsx
+++ client/src/pages/Training.jsx
@@ -1,24 +1,33 @@
-import audioStream from '../utils/audioStream';
+import { Note } from '../utils/audioStream';
 import playNote from '../utils/playNote';
 import { useState } from 'react';
 
 const Training = () => {
+  const currentNote = new Note();
   const [playbackNote, setPlaybackNote] = useState();
 
-  const tuner = () => audioStream();
+  const tuner = () => currentNote.audioStream();
   const note = () => {
     playNote().then((note) => {
       setPlaybackNote(note);
     });
   };
- 
+
+  const noteChecker = () => {
+    console.log(currentNote.noteTracker);
+  };
+
   return (
     <>
       <button onClick={tuner}>Test</button>
       <button onClick={note}>Play This Note</button>
-      <div style={{ color: 'white' }}>Play This Note {playbackNote}</div>
+      <button onClick={noteChecker}>Note Checker</button>
 
-      <div id='note' style={{ color: 'white' }}></div>
+      <div style={{ color: 'white', fontSize: 100 }}>
+        Play This Note {playbackNote}
+      </div>
+
+      <div id='note' style={{ color: 'white', fontSize: 100 }}></div>
     </>
   );
 };
