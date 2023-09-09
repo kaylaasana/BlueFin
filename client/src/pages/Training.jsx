dfacdebbb071cc624e6536d33e5f2ca628c9fc0b
Jay Liu <jayliu89@gmail.com>
2023-09-09 16:03:58 -0700

    created a difficulty selection componenet. still working on timer for training room

--- client/src/pages/Training.jsx
+++ client/src/pages/Training.jsx
@@ -2,11 +2,14 @@ import { Note } from '../utils/audioStream';
 import playNote from '../utils/playNote';
 import { useEffect, useState } from 'react';
 
-const Training = () => {
+const Training = ({ difficulty, setDifficulty }) => {
   const currentNote = new Note();
-  const [playbackNote, setPlaybackNote] = useState();
+
+  const [playbackNote, setPlaybackNote] = useState(' ');
   const [score, setScore] = useState(0);
   const [timer, setTimer] = useState(10);
+  const [interval, setInterval] = useState(null);
+  const [count, setCount] = useState(0);
 
   const checkNote = (interval) => {
     if (playbackNote == currentNote.noteTracker) {
@@ -27,34 +30,67 @@ const Training = () => {
   };
 
   useEffect(() => {
-    currentNote.audioStream();
+    note();
+  }, []);
 
-    let time = 0;
+  useEffect(() => {
+    const id = setInterval(() => {
+      // Your code here
+      console.log('are you counting down?');
+      setCount(count + 1); // Example: Increment count every second
+    }, 1000); // Interval in milliseconds (e.g., 1000ms = 1 second)
+    // Clear the interval when the component unmounts
+    return () => clearInterval(id);
+  }, []);
+
+  useEffect(() => {
+    currentNote.audioStream();
     const interval = setInterval(function () {
-      time++;
-      if (time >= 10) {
-        console.log("time's up");
+      setTimer(timer - 1);
+      if (timer <= 0) {
+        setTimer("Time's Up!");
         clearInterval(interval);
       }
       checkNote(interval);
     }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
   }, [playbackNote]);
 
   return (
     <>
-      <button onClick={tuner}>Test</button>
-      <button onClick={note}>Play This Note</button>
-      {/* <button onClick={testInterval}>Testing Interval, sort of</button> */}
+      {difficulty ? (
+        <>
+          <button onClick={tuner}>Test</button>
+          <button onClick={note}>Play This Note</button>
 
-      <div style={{ color: 'white', fontSize: 100 }}>
-        Play This Note {playbackNote}
-      </div>
-      <div style={{ color: 'white', fontSize: 100 }}>Timeleft: {timer}</div>
-      <div style={{ color: 'white', fontSize: 100 }}>Your Score: {score}</div>
+          <div style={{ color: 'white', fontSize: 100 }}>
+            Play This Note {playbackNote}
+          </div>
+          {/* <div style={{ color: 'white', fontSize: 100 }}>Timeleft: {timer}</div> */}
+          <div style={{ color: 'white', fontSize: 100 }}>
+            Your Score: {score}
+          </div>
 
-      <div id='note' style={{ color: 'white', fontSize: 100 }}></div>
+          <div style={{ color: 'white' }}>{count}</div>
+
+          <div id='note' style={{ color: 'white', fontSize: 100 }}></div>
+        </>
+      ) : (
+        <>
+          <button onClick={tuner}>Test</button>
+          <button onClick={note}>Play This Note</button>
+
+          <div style={{ color: 'white', fontSize: 100 }}>Timeleft: {timer}</div>
+          <div style={{ color: 'white', fontSize: 100 }}>
+            Your Score: {score}
+          </div>
+
+          <div id='note' style={{ color: 'white', fontSize: 100 }}></div>
+        </>
+      )}
     </>
   );
 };
-
 export default Training;
