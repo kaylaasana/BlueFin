fc1bdb1fc5ba0f3e4c0e1a540ae0c4eaa005f8dd
Andy Godoy <Andygodoy@ymail.com>
2023-09-09 15:47:19 -0700

    removed the background to match the 3D model aspect for possible future use, I also fixed the check mark boxes in position so the text character length did not affect placement of it, I also spaced out the logout button from overlapping the homepage button

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -31,31 +31,7 @@ function ProfilePage() {
     { id: 5, name: 'Write your goals here' },
   ]);
 
-  const [isEditing, setIsEditing] = useState(false);
-  const [editedUserInfo, setEditedUserInfo] = useState({
-    username: userData.username,
-    email: userData.email,
-  });
   const [editingGoalId, setEditingGoalId] = useState(null);
-
-  const handleUserInfoSubmit = (e) => {
-    e.preventDefault();
-    setIsEditing(false);
-    setUserData({
-      username: editedUserInfo.username,
-      email: editedUserInfo.email,
-    });
-  };
-
-  // // Update the state with the user data when it's available
-  // useEffect(() => {
-  //   if (!loading && !error && data && data.user) {
-  //     setUserData({
-  //       username: data.user.username,
-  //       email: data.user.email,
-  //     });
-  //   }
-  // }, [loading, error, data]);
 
   const resetProgress = () => {
     setCompletedTasks(0);
@@ -87,54 +63,30 @@ function ProfilePage() {
   };
 
   return (
-    <div>
-      <div className="top-left-buttons">
+    <div className="profile-page-container">
+      
         <Link to="/" className="homepage-button">
           Homepage
         </Link>
-      </div>
-
+     
       <div>
-        <button onClick={Auth.logout}>Logout</button>
+        <button onClick={Auth.logout} className="logout-button">
+          Logout
+        </button>
       </div>
-
+  
       <div className="profile-container">
         <div className="profile-info">
           <h2>{userData.username}'s Profile</h2>
           <p>Email: {userData.email}</p>
-          {isEditing ? (
-            <form onSubmit={handleUserInfoSubmit}>
-              <input
-                type="text"
-                name="username"
-                value={editedUserInfo.username}
-                onChange={(e) =>
-                  setEditedUserInfo({ ...editedUserInfo, username: e.target.value })
-                }
-              />
-              <input
-                type="text"
-                name="email"
-                value={editedUserInfo.email}
-                onChange={(e) =>
-                  setEditedUserInfo({ ...editedUserInfo, email: e.target.value })
-                }
-              />
-              <button type="submit" className="button">
-                Save
-              </button>
-            </form>
-          ) : (
-            <button onClick={() => setIsEditing(true)} className="button">
-              Edit Info
-            </button>
-          )}
         </div>
         <div className="progression">
           <div className="progress-bar-container">
             <div
               className="progress-bar"
-              style={{ width: `${Math.floor((completedTasks / totalTasks) * 100)}%` }}
+              style={{
+                width: `${Math.floor((completedTasks / totalTasks) * 100)}%`,
+              }}
             >
               <span className="progress-text">
                 {Math.floor((completedTasks / totalTasks) * 100)}%
@@ -145,19 +97,21 @@ function ProfilePage() {
             Reset Progress
           </button>
         </div>
-
+  
         <div className="goals-container">
           <div className="fixed-goals-box">
             <h3>Goals and Objectives</h3>
             <ul style={{ listStyleType: 'none' }}>
               {goals.map((goal) => (
-                <li key={goal.id} style={{ marginBottom: '20px' }}>
+                <li key={goal.id} className="goal-item">
                   {editingGoalId === goal.id ? (
                     <>
                       <input
                         type="text"
                         value={goal.name}
-                        onChange={(e) => handleGoalEdit(goal.id, e.target.value)}
+                        onChange={(e) =>
+                          handleGoalEdit(goal.id, e.target.value)
+                        }
                       />
                       <button onClick={saveGoalEdit} className="button">
                         Save
@@ -172,9 +126,7 @@ function ProfilePage() {
                         className="checkbox"
                       />
                       <span
-                        onClick={() => startEditingGoal(goal.id)}
-                        style={{ cursor: 'pointer', marginLeft: '25px' }}
-                      >
+                        onClick={() => startEditingGoal(goal.id)} className="goal-name">
                         {goal.name}
                       </span>
                     </>
