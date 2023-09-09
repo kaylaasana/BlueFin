2deb006d79c747814b7139b0f7fe39a05897d250
kayla <kayladatte@gmail.com>
2023-09-09 13:10:51 -0700

    added and formatted error text for no input on sign up form

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -6,34 +6,54 @@ import Auth from "../utils/auth";
 import { validateEmail } from "../utils/helpers";
 
 const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [formState, setFormState] = useState({
     username: "",
     email: "",
     password: "",
   });
   const [createUser, { error, data }] = useMutation(CREATE_USER);
-  const [errorMessage, setErrorMessage] = useState("");
+  // const [userMessage, setUserErrorMessage] = useState("");
+  // const [emailMessage, setEmailErrorMessage] = useState("");
+  // const [passwordMessage, setPasswordErrorMessage] = useState("");
+  // const [formMessage, setFormErrorMessage] = useState("");
+  const [errorMessages, setErrorMessages] = useState([]);
 
   // update the state based on the form input changes
   const handleChange = (event) => {
-    const { name, value } = event.target;
+    // const { name, value } = event.target;
+    const { target } = event;
+    const inputType = target.name;
+    const inputValue = target.value;
 
     //set form state based on the value
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
+    // setFormState({
+    //   ...formState,
+    //   [name]: value,
+    // });
+
+    // check input type
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "username") {
+      setUsername(inputValue);
+    } else {
+      setPassword(inputValue);
+    }
   };
 
   //form submit
   const handleFormSubmit = async (event) => {
     // check if email input is a valid email
     event.preventDefault();
+    const errors = [];
     if (email && !validateEmail(email)) {
-      setErrorMessage("email is invalid");
+      errors.push("email is invalid");
       return;
     }
-    
+
     // assign formState to data
     try {
       const { data } = await createUser({
@@ -49,19 +69,50 @@ const SignUp = () => {
 
     // reset input fields on submission
     setFormState({
-      username:"",
+      username: "",
       email: "",
       password: "",
     });
+
+    setErrorMessages([]);
+
   };
 
   //  alert the user if there is no input
-  const noInput = (e) => {
-    e.preventDefault();
-    if (!username || !email || !password) {
-      setErrorMessage("all fields required");
-      return;
+  // const noInput = (event) => {
+  //   event.preventDefault();
+  //   if (!username && !email && !password){
+  //     setFormErrorMessage("username, email and password required")
+  //   }
+  //   if (!username) {
+  //     setUserErrorMessage("username required");
+  //     return;
+  //   }
+  //   if (!email) {
+  //     setEmailErrorMessage("email required");
+  //     return;
+  //   }
+  //   if (!password) {
+  //     setPasswordErrorMessage("password required");
+  //     return;
+  //   }
+  // };
+
+  const noInput = (event) => {
+    event.preventDefault();
+    const errors = [];
+
+    if (!username) {
+      errors.push("Username is required");
     }
+    if (!email) {
+      errors.push("Email is required");
+    }
+    if (!password) {
+      errors.push("Password is required");
+    }
+
+    setErrorMessages(errors);
   };
 
   return (
@@ -70,10 +121,12 @@ const SignUp = () => {
         <Link to="/">
           <button>Homepage</button>
         </Link>
-        <Link to='/login'><button>Log In</button></Link>
+        <Link to="/login">
+          <button>Log In</button>
+        </Link>
       </div>
       <div className="col d-flex justify-content-center">
-        <form onSubmit={handleFormSubmit}>
+        <form onSubmit={handleFormSubmit} onBlur={noInput}>
           <label className="row text">
             Username
             <br></br>
@@ -82,8 +135,9 @@ const SignUp = () => {
               placeholder="username"
               name="username"
               type="username"
-              value={formState.username}
+              value={username}
               onChange={handleChange}
+              onBlur={noInput}
             />
           </label>
           <label className="row">
@@ -94,8 +148,9 @@ const SignUp = () => {
               placeholder="your email"
               name="email"
               type="email"
-              value={formState.email}
+              value={email}
               onChange={handleChange}
+              onBlur={noInput}
             />
           </label>
           <label className="row">
@@ -106,19 +161,31 @@ const SignUp = () => {
               placeholder="********"
               name="password"
               type="password"
-              value={formState.password}
+              value={password}
               onChange={handleChange}
+              onBlur={noInput}
             />
           </label>
           <button type="submit">Submit</button>
         </form>
 
-        {errorMessage && (
+        {/* {[userMessage, passwordMessage, emailMessage, formMessage] && (
+          <div>
+            <p className="error-text">{[userMessage, passwordMessage, emailMessage, formMessage]}</p>
+          </div>
+        )} */}
+      </div>
+      {errorMessages.length > 0 && (
         <div>
-          <p className="error-text">{errorMessage}</p>
+          <ul className="error-list row p-3">
+            {errorMessages.map((message, index) => (
+              <li key={index} className="error-text d-flex justify-content-center">
+                {message}
+              </li>
+            ))}
+          </ul>
         </div>
       )}
-      </div>
     </div>
   );
 };
